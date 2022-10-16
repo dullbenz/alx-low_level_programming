792e15c62d31c400dbd1a2a19e8cfcebfc1f9489
LOT-BILL FUASHI <lotbill.fuashi@gmail.com>
2022-10-16 13:27:29 -0700

    Using formatted print i.e printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the entry point of the application
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
