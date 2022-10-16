765bb22000f8ebbe38b5b820092f9ed4d78fe536
LOT-BILL FUASHI <lotbill.fuashi@gmail.com>
2022-10-16 13:23:20 -0700

    Formatted c file in betty format

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: This is the entry point for our application
+ *
+ * Return: Always 0 (for success)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
