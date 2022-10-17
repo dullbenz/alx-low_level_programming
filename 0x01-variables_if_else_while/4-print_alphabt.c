6df44e19c665eef62382a99e3c3273accb588408
LOT-BILL FUASHI <lotbill.fuashi@gmail.com>
2022-10-17 04:09:33 -0700

    Play with alphabet, remove q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	do {
+		if (c != 'q' && c != 'e')
+			putchar(c);
+		c++;
+	} while (c <= 'z');
+
+	putchar('\n');
+	return (0);
+}
