63bbdd427d2f13e1e5c211c56d1d50e1651c2458
LOT-BILL FUASHI <lotbill.fuashi@gmail.com>
2022-10-20 16:32:23 -0700

    Compute the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+/**
+ * _abs - Computes the absolute value of an integer
+ *
+ * @n: The number for which to find the absolute value
+ *
+ * Return: The absolute value of n
+ */
+int _abs(int n)
+{
+	return (n < 0 ? -n : n);
+}
