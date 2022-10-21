a54227f7c1b90dee6ddfeeef6553ef61f12499df
LOT-BILL FUASHI <lotbill.fuashi@gmail.com>
2022-10-20 17:41:02 -0700

    Add 2 integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+/**
+ * add - Add two integers and return the result
+ *
+ * @a: The fist number
+ *
+ * @b: The second number
+ *
+ * Return: The sum (a+b)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
