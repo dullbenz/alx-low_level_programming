a2b060ebee2028cfa9ede927a935d85ee5ecae22
LOT-BILL FUASHI <lotbill.fuashi@gmail.com>
2022-10-23 00:54:20 -0700

    Print first 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,23 +7,38 @@
  */
 int main(void)
 {
-	unsigned long int prev, temp, next;
-	int i;
+	unsigned long int prev, temp, next, max_long_u, mod;
+	int i, prev_t, next_t, temp_t;
 
 	prev = 1;
 	next = 2;
+	prev_t = 0;
+	next_t = 0;
+	max_long_u = 99999999999999999;
+	mod = max_long_u + 1;
 
 	printf("1, 2, ");
 
-	for (i = 0; i < 95; i++)
+	for (i = 0; i < 96; i++)
 	{
 		temp = next;
+		temp_t = next_t;
+
 		next += prev;
+		next_t += prev_t + (int) (next / mod);
+		next = next % mod;
+
+		prev_t = temp_t;
 		prev = temp;
 
-		printf("%lu, ", next);
+		if (next_t > 0)
+		{
+			printf("%d", next_t);
+			printf("%017lu%s", next, i == 95 ? "\n" : ", ");
+		} else
+		{
+			printf("%lu%s", next, i == 95 ? "\n" : ", ");
+		}
 	}
-
-	printf("%lu\n", prev + next);
 	return (0);
 }
