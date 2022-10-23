a391d578138b941130171c7b65d52551e5a2da59
LOT-BILL FUASHI <lotbill.fuashi@gmail.com>
2022-10-22 23:45:07 -0700

    Print even fibonacci numbers that do not exceed 3524578

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Print the first even fibonacci numbers <= 40000000
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int prev, temp, next;
+
+	prev = 1;
+	next = 2;
+
+	while (next <= 4000000)
+	{
+		if (next % 2 == 0)
+			if (next + prev > 4000000)
+				printf("%d\n", next);
+			else
+				printf("%d, ", next);
+		temp = next;
+		next += prev;
+		prev = temp;
+	}
+	return (0);
+}
