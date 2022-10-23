93a0357ce923919c162f785a585ed7cbb2ce74cb
LOT-BILL FUASHI <lotbill.fuashi@gmail.com>
2022-10-22 23:51:56 -0700

    Fix even fibonacci to print sum rather than the numbers themselves

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 
 /**
- * main - Print the first even fibonacci numbers <= 40000000
+ * main - Print the sum of the first even fibonacci numbers <= 40000000
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int prev, temp, next;
+	unsigned long int ans;
 
 	prev = 1;
 	next = 2;
+	ans = 0;
 
 	while (next <= 4000000)
 	{
 		if (next % 2 == 0)
-		{
-			if (next + prev > 4000000)
-				printf("%d\n", next);
-			else
-				printf("%d, ", next);
-		}
+			ans += next;
 		temp = next;
 		next += prev;
 		prev = temp;
 	}
+
+	printf("%lu\n", ans);
 	return (0);
 }
